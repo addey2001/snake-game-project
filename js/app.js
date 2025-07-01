59ae4b1783ab8bdcefd2578f557061cdfa74cc19
addey2001 <addey2001>
2025-07-01 14:24:24 +0100

    refactored broken code

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const cellElements = []
 const scoreDisplay = document.getElementById('score')
 const TimeDisplay = document.getElementById('time')
 const board = document.getElementById('board')
-const startPosition = []
+const startPosition = [188,189,190]
 
 //variables 
 let currentPosition = startPosition
@@ -17,14 +17,15 @@ let foodPosition = null;
 let gameOver = false;
 let time = 0;
 let score = 0;
-let snake = [startingPosition]
+let snake = [...startPosition]
 let intervalId = null;
 let timeIntervalId = null
 
 
 //function
-
-function creatBoard () {
+function addSnake() {
+}
+function createBoard () {
     for( let i = 0; i < cellCount; i++) { // used a loop 
     const cell = document.createElement('div'); // created a new div for cells
     cell.innerText = i;
@@ -32,8 +33,9 @@ function creatBoard () {
     cell.style.width = `${100 / columns}%`; // for the columns 
     cell.style.height =`${100 / rows}%`;// 
     cell.classList.add('cell');
-    cellElements.push('cell');
-    board.appendChild('cell');
+    cellElements.push(cell);
+    board.appendChild(cell
+    );
  }}
 ;
 
@@ -51,32 +53,13 @@ function updateBoard() {
     cellElements.forEach(cell => cell.className = 'cell') // this resets each cell className to cell, and makes sure its in a default state removing any other classes that may have been there previously
 snake.forEach(idx => cellElements[idx].classList.add('snake')) //iteratiing over array calling it 'snake' adding its classList to the cell
 if (typeof foodPosition === 'number') {
-    cellElements[foodposition].classList.add('food')// this adds the name 'food' to each area where the food is randomly place onn the game board 
+    cellElements[foodPosition].classList.add('food')// this adds the name 'food' to each area where the food is randomly place onn the game board 
 }
 
 }
 
 function startGame() {
-    if( gameOver === false) return;
-    gameOver = false
-    score = 0
-    time = 0 
-    snakeLength = 1
-    snakeSpeed = 200
-    snake = {startingPosition}
-    currentPosition= startingPosition;
-    scoreDisplay.textContent = 'score: 0'
-    TimeDisplay.textContent = 'time: 0'
-    playButton.disabled = true ;
-    placefoodRdm()
-    updateBoard();
-    clearInterval(intervalId);
-    intervalId = setInterval(moveSnake, snakeSpeed);
-    clearInterval(timeIntervalId);
-    timeIntervalId = setInterval (() => { 
-        time++;
-        TimeDisplay.textContent = `Time: ${time}`;
-    }, 1000);
+  
 }
 
 function moveSnake(event) {
@@ -94,4 +77,10 @@ function moveSnake(event) {
 
 
 
-document.addEventListener('click', startGame)
+playButton.addEventListener('click', startGame)
+
+console.log(playButton)
+
+// on page load 
+createBoard()
+addSnake()
